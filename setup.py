4fce89d0086bd14dcb5a4f1d50de1c4de77b5bf1
Tanisha Jain <itanishajain@gmail.com>
2025-01-09 21:45:29 +0530

    Update readme.md file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="streamix",
-    version="5.0",
+    version="6.0",
     packages=find_packages(),
     install_requires=[
         "yt-dlp",
